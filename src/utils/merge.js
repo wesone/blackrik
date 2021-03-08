a70fb0dd63ca5e9df62a35dd5e40b4007e32336b
wesone <github@wesone.de>
2021-03-08 15:53:53 +0100

    refactored utils/merge

--- src/utils/merge.js
+++ src/utils/merge.js
@@ -1,7 +1,5 @@
 const deepmerge = require('deepmerge');
 
 module.exports = (...objects) => deepmerge.all(objects, {
-    isMergeableObject: obj => {
-        return !!(obj !== undefined && obj !== null && obj.constructor === Object || Array.isArray(obj));
-    }
+    isMergeableObject: obj => !!(obj !== undefined && obj !== null && obj.constructor === Object || Array.isArray(obj))
 });
