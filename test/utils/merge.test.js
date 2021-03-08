cebc12daf5cf2158200455171af61ba7d42b7171
Kevin Laube <k.laube@synatix.com>
2021-03-08 15:13:34 +0100

    merge test

--- test/utils/merge.test.js
+++ test/utils/merge.test.js
@@ -1,8 +1,12 @@
 import merge from '../../src/utils/merge';
 
-test('Check if the Objects can be merged', () => {
+test('Merge two objects', () => {
     const obj1 = { test1: 1 };
     const obj2 = { test2: 'test2' };
-    const mergable = merge([obj1, obj2]);
-    expect(mergable).toBe(true);
+    const expected = {
+        test1: 1,
+        test2: 'test2'
+    };
+    const merged = merge(obj1, obj2);
+    expect(merged).toEqual(expected);
 });
