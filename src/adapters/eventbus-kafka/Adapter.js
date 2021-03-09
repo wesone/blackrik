b2aa3648b3f80f9b61e4e57c1b7b55ab922190e1
wesone <github@wesone.de>
2021-03-09 14:18:37 +0100

    updated kafka eventbus adapter; updated interface error message

---
+++ src/adapters/eventbus-kafka/Adapter.js
@@ -0,0 +1,21 @@
+const EventBusAdapterInterface = require('../EventBusAdapterInterface');
+
+class Adapter extends EventBusAdapterInterface
+{
+    constructor(args)
+    {
+        super();
+    }
+
+    subscribe()
+    {
+
+    }
+
+    publish()
+    {
+
+    }
+}
+
+module.exports = Adapter;
