afe9bceae3a27b1b4952c0050f23b19cec49468c
snizzl <klaube1@gmx.de>
2021-03-16 09:31:08 +0100

    eventstore save

--- test.js
+++
@@ -1,7 +0,0 @@
-const config = { host: 'localhost', port: 3306, user: 'root', password: '1234', database: 'eventStore' };
-const eventStoreAdapter = require('./src/adapters/eventstore-mysql');
-
-(async () => {
-    const eventStore = eventStoreAdapter(config);
-    await eventStore.init();
-})();
