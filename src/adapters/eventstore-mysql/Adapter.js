d300fdfb2e316a92f902b45cc42f708206ed6685
wesone <github@wesone.de>
2021-03-16 14:43:48 +0100

    added workaround for mysql2 module with MySQL 8.0.22 error

--- src/adapters/eventstore-mysql/Adapter.js
+++ src/adapters/eventstore-mysql/Adapter.js
@@ -40,7 +40,7 @@ class Adapter extends EventStoreAdapterInterface
 
         // see https://github.com/sidorares/node-mysql2/issues/1239
         //========MySQL 8.0.22 (and higher) fix========
-        const originalExecute = this.db.execute;
+        const originalExecute = this.db.execute.bind(this.db);
         this.db.execute = function(...args){
             const [query, substitutions, ...rest] = args;
             for(const key in substitutions) // array or object
