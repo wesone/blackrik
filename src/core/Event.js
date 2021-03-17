1eb1c77259c20b7756b2b1fdfd14f72519f5707e
wesone <github@wesone.de>
2021-03-17 09:00:24 +0100

    implemented event causation

--- src/core/Event.js
+++ src/core/Event.js
@@ -1,15 +1,48 @@
+const {v4: uuid} = require('uuid');
+
 class Event
 {
-    constructor({aggregateId, type, correlationId, causationId, payload})
+    #data;
+
+    static from(data)
     {
-        return {
+        const event = new this(data);
+        if(data.id && data.id.length)
+            event.#data.id = data.id;
+        if(!isNaN(data.timestamp) && data.timestamp > 0)
+            event.#data.timestamp = data.timestamp;
+        return event;
+    }
+
+    constructor(data)
+    {
+        const {
+            aggregateId,
+            type,
+            correlationId = null,
+            causationId = null,
+            payload = null
+        } = data; 
+
+        const id = uuid();
+        this.#data = {
+            id,
             aggregateId,
             type,
             timestamp: Date.now(),
-            correlationId,
+            correlationId: correlationId || id,
             causationId,
             payload
         };
+
+        Object.keys(this.#data).forEach(key => Object.defineProperty(this, key, {
+            get: () => this.#data[key]
+        }));
+    }
+
+    toJSON()
+    {
+        return this.#data;
     }
 }
 
