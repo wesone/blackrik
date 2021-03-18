642145791644bb7a95682fda19aa6abf231b21d9
Hendrik Westerberg <55196856+hvipana@users.noreply.github.com>
2021-03-18 16:08:59 +0100

    Updated example

--- examples/hello-world/readModels/users.resolvers.js
+++ examples/hello-world/readModels/users.resolvers.js
@@ -1,8 +1,24 @@
 
 const tableName = 'users';
 
+async function getLastPosition(store)
+{
+    return (await store.findOne(tableName,{
+        fields: ['lastPosition'],
+        order: {lastPosition: 1}
+    }))?.lastPosition ?? -1;
+}
+
 module.exports = {
     get: async (store, args) => {
+        if(args?.position)
+        {
+            const lastPosition = await getLastPosition(store);
+            if(args.position > lastPosition)
+            {
+                throw new Error('Data not yet availible');
+            }
+        }
         return await store.findOne(tableName, {
             conditions:{id: args.id}
         });
