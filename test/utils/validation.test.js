e5f9c438ec7556e1dd5b9b9db6f8c1a86ef420c5
Martin Kraft <m.kraft@synatix.com>
2021-03-19 15:33:13 +0100

    validation test update

--- test/utils/validation.test.js
+++ test/utils/validation.test.js
@@ -1,6 +1,35 @@
 const {validateConfig} = require('../../src/utils/validation');
-const template = require('../../examples/hello-world/config');
+const configs = require('../../examples/hello-world/config');
+const merge = require('../../src/utils/merge');
+const defaultAdapters = Object.freeze(require('../../src/adapters'));
+const config = {
+    aggregates: [],
+    readModels: [],
+    sagas: [],
+    readModelStoreAdapters: {
+        default: {
+            module: defaultAdapters.READMODELSTORE.MySQL
+        }
+    },
+    eventStoreAdapter: {
+        module: defaultAdapters.EVENTSTORE.MySQL
+    },
+    eventBusAdapter: {
+        module: defaultAdapters.EVENTBUS.Kafka,
+        args: {
+            clientId: 'blackrik-application'
+        }
+    },
+    server: {
+        config: {
+            port: 3000,
+            skipDefaultMiddlewares: false
+        },
+        middlewares: [],
+        routes: []
+    }
+};
 
 test('Validate config scheme', () => {
-    expect(() => validateConfig(template)).not.toThrow();
+    expect(() => validateConfig(merge(config, configs))).not.toThrow();
 });
