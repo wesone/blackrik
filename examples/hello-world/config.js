3aad2e379ed90f13485208e007d6663bc7ce9d06
wesone <github@wesone.de>
2021-03-19 14:04:10 +0100

    renamed exampleInstance to config; fixed eventstore args

---
+++ examples/hello-world/config.js
@@ -0,0 +1,71 @@
+module.exports = {
+    aggregates: [
+        {
+            name: 'user',
+            commands: require('./aggregates/user.commands'),
+            projection: require('./aggregates/user.projection')
+        }
+    ],
+    readModels: [
+        {
+            name: 'users',
+            projection: require('./readModels/users.projection'),
+            resolvers: require('./readModels/users.resolvers'),
+            adapter: 'default'
+        }
+    ],
+    sagas: [
+        {
+            name: 'saga-users',
+            source: require('./sagas/users'),
+            adapter: 'default'
+        }
+    ],
+    readModelStoreAdapters: {
+        default: {
+            args: {
+                debugSql: true,
+                host: 'localhost',
+                database: 'readmodelstore',
+                user: 'root',
+                password: '1234'
+            }
+        }
+    },
+    eventStoreAdapter: {
+        args: {
+            host: 'localhost',
+            database: 'eventstore',
+            user: 'root',
+            password: '1234'
+        }
+    },
+    eventBusAdapter: {
+        args: {
+            brokers: ['localhost:9092']
+        }
+    },
+    server: {
+        config: {
+            port: 3000,
+            skipDefaultMiddlewares: false
+        },
+        middlewares: [
+            (req, res, next) => next(),
+            [
+                '/middleware/test',
+                (req, res, next) => next(),
+                (req, res, next) => next()
+            ]
+        ],
+        routes: [
+            {
+                method: 'GET',
+                path: '/test',
+                callback: (req, res) => {
+                    console.log('CALLED /TEST');
+                }
+            }
+        ]
+    }
+};
