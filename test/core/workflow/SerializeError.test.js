f61caf547d65687f415b49d503e454bd53bb2cc3
Hendrik Westerberg <55196856+hvipana@users.noreply.github.com>
2021-04-14 08:47:06 +0200

    Updated SerializeError tests

--- test/core/workflow/SerializeError.test.js
+++ test/core/workflow/SerializeError.test.js
@@ -1,7 +1,7 @@
 const {serializeError, deserializeError} = require('../../../src/core/workflow/SerializeError');
 
 const error = new Error('Test');
-error.test = {test2: 'Error', error, fn: () => {}, buf: Buffer.alloc(1)};
+error.test = {test2: 'Error', error, fn: () => {}, buf: Buffer.alloc(1), errors: [error]};
 
 test('SerializeError', () => {
     expect(serializeError(1)).toEqual(1);
@@ -11,3 +11,41 @@ test('SerializeError', () => {
     expect(deserializeError(error)).toEqual(error);
     expect(deserializeError(1)).toBeInstanceOf(Error);
 });
+
+test('toJSON', () => {
+    class CustomError extends Error {
+        constructor(value){
+            super('foo');
+            this.name = this.constructor.name;
+            this.value = value;
+        }
+    }
+    const value = {
+        amount: 20,
+        toJSON(){
+            return {
+                amount: `$${this.amount}`
+            };
+        }
+    };
+    const error = new CustomError(value);
+    const serialized = serializeError(error);
+    const {stack, ...rest} = serialized;
+
+    expect(rest).toMatchObject({
+        message: 'foo',
+        name: 'CustomError',
+        value: {
+            amount: '$20'
+        }
+    });
+
+    expect(stack).toBeDefined();
+});
+
+test('toJSON Error', () => {
+    const deserialized = deserializeError(BigInt(1));
+
+    expect(deserialized).toBeInstanceOf(Error);
+});
+
